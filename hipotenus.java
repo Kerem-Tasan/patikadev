48845d15cc4459a9649e131164fddfd3fe16d12e
Kerem-Tasan <128915083+Kerem-Tasan@users.noreply.github.com>
2023-03-28 15:06:33 +0300

    Add files via upload

---
+++ hipotenus.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class hipotenus {
+    public static void main(String[] args) {
+        Scanner inp=new Scanner(System.in);
+       
+        //üçgenin hipotenüsünü bulan program
+  
+  int hipo;
+
+  System.out.println("a kenarını giriniz:");
+  int kenar2=verial.nextInt();
+
+  System.out.println("b kenarını giriniz:");
+  int kenar1=verial.nextInt();
+  
+  hipo=kenar1*kenar1+kenar2*kenar2;
+  System.out.println("üçgenin hipotenüsü="+hipo);
+    }
+}
